27b04634f94696b8739207ec9f8d3380237f8f77
JiangJian <1548063745@qq.com>
2021-05-24 16:38:01 +0800

    'initproject'

---
+++ src/main/java/com/jiangjian/App.java
@@ -0,0 +1,13 @@
+package com.jiangjian;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
